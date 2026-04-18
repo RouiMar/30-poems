179e9c3962199c99c9d45c70e2e89ce32bb8692d
RouiMar <rouisalalima12@gmail.com>
2026-04-18 22:42:21 +0800

    Fix book

--- script.js
+++ script.js
@@ -3,29 +3,36 @@
 const pageFlip = new St.PageFlip(
   document.getElementById("book"),
   {
-    width: 900,
+    width: 420,
     height: 600,
     size: "fixed",
     maxShadowOpacity: 0.5,
     showCover: true,
-    mobileScrollSupport: false
+      drawshadow: true,
+    mobileScrollSupport: false,
+    useMouseEvents: true
   }
 );
 
 pageFlip.loadFromHTML(document.querySelectorAll(".page"));
 
-/* TEXT ANIMATION ON FLIP */
+/* TEXT ANIMATION ON PAGE TURN */
 pageFlip.on("flip", (e) => {
-  const currentPage = e.data;
-
   const pages = document.querySelectorAll(".page");
+  const page = pages[e.data];
 
-  if (pages[currentPage]) {
-    gsap.from(pages[currentPage].querySelectorAll("p, h2, h3"), {
-      opacity: 0,
-      y: 20,
-      duration: 1,
-      stagger: 0.2
-    });
-  }
+  if (!page) return;
+
+  // ❌ Skip animation for chapter pages
+  if (page.classList.contains("chapter")) return;
+  if (page.classList.contains("ending")) return;
+  if (page.classList.contains("message")) return;
+
+  gsap.from(page.querySelectorAll("p, h2, span"), {
+    opacity: 0,
+    y: 20,
+    duration: 1,
+    stagger: 0.2,
+    ease: "power2.out"
+  });
 });
